1368934b83240ca2283813aae893a114e3d9a924
Mugisha-isaac <mugishaisaac2001@gmail>
2022-05-02 15:49:29 +0200

    fix(app.ts) resolving  routing issues

--- controllers/admin.controller.ts
+++ controllers/admin.controller.ts
@@ -1,37 +1,4 @@
 
-// import express from 'express';
-// import * as jwt from 'jsonwebtoken';
-// import adminService from '../services/admin.service';
-
-// class AdminController{
-//      createNewAdminController = async(req:express.Request,  res:express.Response)=>{
-//         const admin = await adminService.createNewAdmin(req.body.username,req.body.password,req.body.isAdmin);
-//         if(!admin) return res.status(400).send({message:"Failed to create new admin"});
-//         return res.status(201).send(admin);
-//     }
-
-//     adminLoginController = async(req:express.Request,res:express.Response)=>{
-//         const admin = await adminService.Login(req.body.username,req.body.password);
-//         if(!admin) return res.status(300).send({message:"invalid credentials", success:false});
-//         const admin_id = admin._id;
-//         let payload = {admin_id};
-//         let token = jwt.sign(payload,"swsh23hjddnns",{
-//             algorithm: "HS256",
-//             expiresIn:86400
-//         })
-
-//         res.cookie("jwt",token,{secure:true, httpOnly:true});
-//         return res.status(200).send({
-//             accessToken:token,
-//             message:"Successful Login", 
-//             success:true,
-//             data: admin
-//         })
-//     }
-// }
-
-// export default  AdminController;
-
 import {Request,Response,NextFunction, Router} from 'express';
 import NotAuthorisedTokenException from '../Exceptions/NotAuthorisedException';
 import Controller from '../interfaces/controller.interface';
@@ -41,6 +8,7 @@ import Admin from '../model/admin.model';
 import UsersNotFoundException from '../Exceptions/UsersNotFoundException';
 import UserNotFoundException from '../Exceptions/UserNotFoundException'
 import SavingNewUserFailedException from '../Exceptions/SavingNewUserFailedException';
+import CreateAdminDto from '../admin/admin.dto';
 
 class AdminController{
     public path ='/admin';
@@ -52,6 +20,7 @@ class AdminController{
     }
 
     private initialiseRoutes(){
+    
         this.router.get(`${this.path}/`, authMiddleware, this.getAllAdmins);
         this.router.get(`${this.path}/:id`, authMiddleware, this.getAdminById);
         this.router.post(`${this.path}/register`, this.createAdmin)
@@ -71,16 +40,14 @@ class AdminController{
     }
 
     private createAdmin = async(request:Request,response:Response, next:NextFunction) =>{
-        console.log('request....', request.body.username)
+         const newAdmin = await this.admin.create({
+             username: request.body.username,
+             password: request.body.password,
+             isAdmin: request.body.isAdmin
+         });
 
-        const newAdmin =  this.admin.create({
-            username:request.body.username,
-            password:request.body.password,
-            isAdmin:request.body.isAdmin
-        });
-
-        if(newAdmin) return response.status(201).send(newAdmin);
-        next(new SavingNewUserFailedException('admin'))
+         if(newAdmin) return response.status(201).send({success:true, message:"Admin successfully created", data: newAdmin});
+         next(new SavingNewUserFailedException('admin'));
     }
 }
 
